33c5e332ad7eaeff14c26893a28038d7a09127fc
Josh <MechJosh0@users.noreply.github.com>
2025-08-31 18:07:18 +0100

    middleware

--- src/features/user/api/userGet.server.ts
+++ src/features/user/api/userGet.server.ts
@@ -1,18 +1,11 @@
 import { createServerFn } from '@tanstack/react-start';
-import { getCookie } from '@tanstack/react-start/server';
 import { userService } from '@/services/user.service';
-import { sessionService } from '@/services/session.service';
+import { isAuthenticatedMiddleware } from '@/middleware/isAuthenticated';
 
-export const userGet = createServerFn({ method: 'GET' }).handler(async () => {
-  // TODO move this into a middleware
-  const sessionToken = getCookie('session');
-  if (!sessionToken) throw new Error('Invalid session');
+export const userGet = createServerFn({ method: 'GET' })
+  .middleware([isAuthenticatedMiddleware])
+  .handler(async ({ context }) => {
+    const user = await userService.profile(context.session.userId);
 
-  const session = await sessionService.extend(sessionToken);
-  if (!session) throw new Error('Invalid session');
-
-  const user = await userService.profile(session.userId);
-
-  console.log('return user', user);
-  return user;
-});
+    return user;
+  });
--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -1,4 +1,23 @@
-export const isAuthenticatedMiddleware = () => {
-  // TODO server auth check
-  return false;
-};
+import { redirect } from '@tanstack/react-router';
+import { createMiddleware } from '@tanstack/react-start';
+import { getCookie } from '@tanstack/react-start/server';
+import { sessionService } from '@/services/session.service';
+
+export const isAuthenticatedMiddleware = createMiddleware({ type: 'function' }).server(async ({ next, context }) => {
+  const sessionToken = getCookie('session');
+  let session;
+
+  if (sessionToken) {
+    session = await sessionService.extend(sessionToken);
+  }
+
+  if (!sessionToken || !session) {
+    throw redirect({ to: '/auth/login' });
+  }
+
+  return next({
+    context: {
+      session,
+    },
+  });
+});
